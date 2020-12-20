7f2a218777a3fa9ec187da4916d987ed44b49d4a
Ruslan Orazliev <rorazliev@outlook.com>
2020-12-20 14:57:08 -0500

    add canvas resize event handler

--- src/game.js
+++ src/game.js
@@ -2,5 +2,22 @@ export default class Game {
   constructor (canvasId) {
     this.canvas = document.getElementById(canvasId);
     this.context = this.canvas.getContext('2d');
+
+    this.handleCanvasResize();
+    window.addEventListener('resize', () => this.handleCanvasResize());
+  }
+
+  handleCanvasResize () {
+    let height = window.innerHeight;
+    let width = window.innerWidth;
+
+    if (height <= width) {
+      this.canvas.height = height;
+      this.canvas.width = height;
+    }
+    else {
+      this.canvas.height = width;
+      this.canvas.width = width;
+    }
   }
 }
