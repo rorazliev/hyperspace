175ccef6307c49f2384b507770da93c7c504700c
Ruslan Orazliev <rorazliev@outlook.com>
2020-12-22 10:58:03 -0500

    Add loadImage function

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,16 @@
-export default function randomNumber(min, max) {
+export function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function loadImage(src) {
+  return new Promise((resolve) => {
+    let image = new Image;
+    image.addEventListener('load', () => {
+      resolve(image);
+    });
+    image.addEventListener('error', (error) => {
+      reject(error);
+    })
+    image.src = src;
+  });
 }
