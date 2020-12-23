fd18c58544ccf2df51c6626e8bfe5f7634f94ed1
Ruslan Orazliev <rorazliev@outlook.com>
2020-12-23 10:03:09 -0500

    Upgrade functions for movement

--- src/ship.js
+++ src/ship.js
@@ -5,21 +5,34 @@ export default class Ship extends Body {
     super(image, 0.1);
     this.x = (canvas.width / 2) - (this.width / 2);
     this.y = (canvas.height / 2) - (this.height / 2);
+    this.canvas = canvas;
   }
 
-  moveLeft () {
-    this.x -= 1;
+  moveLeft (speed) {
+    this.x -= speed;
+    if (this.x < 0) {
+      this.x = 0;
+    }
   }
 
-  moveRight () {
-    this.x += 1;
+  moveRight (speed) {
+    this.x += speed;
+    if (this.x + this.width > this.canvas.width) {
+      this.x = this.canvas.width - this.width;
+    }
   }
 
-  moveUp () {
-    this.y -= 1;
+  moveUp (speed) {
+    this.y -= speed;
+    if (this.y < 0) {
+      this.y = 0;
+    }
   }
 
-  moveDown () {
-    this.y += 1;
+  moveDown (speed) {
+    this.y += speed;
+    if (this.y + this.height > this.canvas.height) {
+      this.y = this.canvas.height - this.height;
+    }
   }
 }
