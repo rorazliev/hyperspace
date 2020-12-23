c9e0bc8711a62983b4760c367e58861a59f71945
Ruslan Orazliev <rorazliev@outlook.com>
2020-12-22 20:51:48 -0500

    Integrate player and movement

--- src/game.js
+++ src/game.js
@@ -1,3 +1,5 @@
+import Ship from './ship';
+
 export default class Game {
   constructor (canvasId, resources) {
     this.canvas = document.getElementById(canvasId);
@@ -5,10 +7,32 @@ export default class Game {
 
     this.resources = resources;
 
-    this.resources.forEach(resource => console.log(resource));
-
     this.handleCanvasResize();
     window.addEventListener('resize', () => this.handleCanvasResize());
+
+    this.player = new Ship(resources[1], this.canvas);
+    window.addEventListener('keydown', (event) => this.handleKeydown(event));
+
+    this.timer = setInterval(() => this.animate(), 1000/120);
+  }
+
+  animate () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.draw(this.player);
+  }
+
+  draw (other) {
+    this.context.drawImage(
+      other.image,
+      0,
+      0,
+      other.image.width,
+      other.image.height,
+      other.x,
+      other.y,
+      other.width,
+      other.height
+    );
   }
 
   handleCanvasResize () {
@@ -22,6 +46,23 @@ export default class Game {
     else {
       this.canvas.height = width;
       this.canvas.width = width;
+    }
+  }
+
+  handleKeydown (event) {
+    switch (event.keyCode) {
+      case 37:
+        this.player.moveLeft();
+        break;
+      case 39:
+        this.player.moveRight();
+        break;
+      case 38:
+        this.player.moveUp();
+        break;
+      case 40:
+        this.player.moveDown();
+        break;
     }
   }
 }
