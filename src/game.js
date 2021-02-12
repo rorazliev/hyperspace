e44746f6d87414898756d02c8c4c934f95eaadb5
Ruslan Orazliev <rorazliev@outlook.com>
2021-02-11 20:18:12 -0500

    integrade requestAnimationFrame

--- src/game.js
+++ src/game.js
@@ -1,109 +1,52 @@
-import Ship from './ship';
-import {randomNumber} from './helpers';
+import {DIRECTION} from './direction';
+import {LEVELING} from './leveling';
 import Meteor from './meteor';
+import {randomNumber} from './randomNumber';
+import Ship from './ship';
 import Space from './space';
 
 export default class Game {
-  constructor (canvasId, resources) {
-    this.canvas = document.getElementById(canvasId);
+  constructor (resources) {
+    this.canvas = document.getElementById('canvas');
     this.context = this.canvas.getContext('2d');
+
+    this.resizeCanvas();
 
     this.resources = resources;
 
-    this.handleCanvasResize();
-    window.addEventListener('resize', () => this.handleCanvasResize());
-
     this.player = new Ship(resources[1], this.canvas);
-    window.addEventListener('keydown', (event) => this.handleKeydown(event));
-
     this.meteors = [];
-
     this.space = [
       new Space(this.resources[2], 0),
       new Space(this.resources[2], this.canvas.height)
     ];
 
-    this.score = 0;
+    this.isRunning = false;
+    this.isOver = false;
+
     this.speed = 5;
     this.level = 1;
+    this.score = 0;
 
-    this.start();
+    this.menu();
+    this.listen();
   }
 
-  step () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    this.renderBackground();
-    this.generateMeteors();
-    this.renderMeteors();
-    this.renderPlayer();
-    this.renderStats();
-  }
-
-  draw (other) {
-    this.context.drawImage(
-      other.image,
+  draw () {
+    this.context.clearRect(
       0,
       0,
-      other.image.width,
-      other.image.height,
-      other.x,
-      other.y,
-      other.width,
-      other.height
+      this.canvas.width,
+      this.canvas.height
     );
-  }
 
-  generateMeteors () {
-    if (randomNumber(0, 1000) > 950) {
-      if (this.meteors.length < 15) {
-        this.meteors.push(new Meteor(this.resources[0], 0.1, this.canvas));
-      }
-    }
-  }
-
-  handleCanvasResize () {
-    let height = window.innerHeight;
-    let width = window.innerWidth;
-
-    if (height <= width) {
-      this.canvas.height = height;
-      this.canvas.width = height;
-    }
-    else {
-      this.canvas.height = width;
-      this.canvas.width = width;
-    }
-  }
-
-  handleKeydown (event) {
-    switch (event.keyCode) {
-      case 37:
-      case 65:
-        this.player.moveLeft(this.speed);
-        break;
-      case 39:
-      case 68:
-        this.player.moveRight(this.speed);
-        break;
-      case 38:
-      case 87:
-        this.player.moveUp(this.speed);
-        break;
-      case 40:
-      case 83:
-        this.player.moveDown(this.speed);
-        break;
-    }
-  }
-
-  pause () {
-    clearInterval(this.timer);
-  }
-
-  renderBackground () {
-    this.space[0].update(this.space[1], this.speed, this.canvas.height);
-    this.space[1].update(this.space[0], this.speed, this.canvas.height);
+    this.context.fillStyle = '#10121e';
+    this.context.fillRect(
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
 
     this.space.forEach(space => {
       this.context.drawImage(
@@ -116,33 +59,16 @@ export default class Game {
         space.y,
         this.canvas.width,
         this.canvas.height
-      );
+      )
     });
-  }
 
-  renderMeteors () {
-    this.meteors.forEach(meteor => {
-      meteor.update(this.speed);
-      this.draw(meteor);
-      if (meteor.top >= this.canvas.height) {
-        this.meteors.shift();
-        this.score += 10;
-        if (this.score % 100 === 0) {
-          this.speed += 1;
-          this.level += 1;
-        }
-      }
-      if (this.player.overlaps(meteor)) {
-        this.pause();
-      }
+    this.meteors.forEach((meteor) => {
+      this.renderObject(meteor);
     });
-  }
 
-  renderPlayer () {
-    this.draw(this.player);
-  }
 
-  renderStats () {
+    this.renderObject(this.player);
+
     this.context.font = 'bold 18px Monospace';
     this.context.fillStyle = '#ffffff';
     this.context.textBaseline = 'middle';
@@ -151,7 +77,109 @@ export default class Game {
     this.context.fillText(`Score: ${this.score}`, 20, 48);
   }
 
-  start () {
-    this.timer = setInterval(() => this.step(), 1000/120);
+  loop () {
+    this.update();
+    this.draw();
+
+    if (!this.isOver) {
+      requestAnimationFrame(() => this.loop());
+    }
+  }
+
+  listen () {
+    document.addEventListener('keydown', (key) => {
+      if (!this.isRunning) {
+        this.isRunning = true;
+        requestAnimationFrame(() => this.loop());
+      }
+
+      if (key.keyCode == 38 || key.keyCode == 87) {
+        this.player.direction = DIRECTION.UP;
+      }
+      if (key.keyCode == 40 || key.keyCode == 83) {
+        this.player.direction = DIRECTION.DOWN;
+      }
+      if (key.keyCode == 37 || key.keyCode == 65) {
+        this.player.direction = DIRECTION.LEFT;
+      }
+      if (key.keyCode == 39 || key.keyCode == 68) {
+        this.player.direction = DIRECTION.RIGHT;
+      }
+    });
+
+    document.addEventListener('keyup', (key) => {
+      this.player.direction = DIRECTION.IDLE;
+    });
+  }
+
+  menu () {
+    this.draw();
+
+    this.context.font = 'bold 18px Monospace';
+    this.context.fillStyle = '#ffffff';
+    this.context.textBaseline = 'middle';
+    this.context.textAlign = 'center';
+    this.context.fillText('Use arrows to move',
+      this.canvas.width / 2,
+      this.canvas.height - 76
+    );
+  }
+
+  renderObject (body) {
+    this.context.drawImage(
+      body.image,
+      0,
+      0,
+      body.image.width,
+      body.image.height,
+      body.x,
+      body.y,
+      body.width,
+      body.height
+    )
+  }
+
+  resizeCanvas () {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+
+    if (height <= width) {
+      this.canvas.height = height;
+      this.canvas.width = height;
+    }
+    else {
+      this.canvas.height = width;
+      this.canvas.width = width;
+    }
+  }
+
+  update () {
+    this.space[0].update(this.space[1], this.speed, this.canvas.height);
+    this.space[1].update(this.space[0], this.speed, this.canvas.height);
+
+    if (randomNumber(0, 1000) > 950) {
+      if (this.meteors.length < 15) {
+        this.meteors.push(new Meteor(this.resources[0], 0.1, this.canvas));
+      }
+    }
+
+    this.meteors.forEach(meteor => {
+      meteor.move(this.speed);
+      if (meteor.top >= this.canvas.height) {
+        this.meteors.shift();
+        this.score += 10;
+      }
+      if (this.player.overlaps(meteor)) {
+        this.isOver = true;
+      }
+    });
+
+    LEVELING.forEach(level => {
+      if (this.score == level.score) {
+        this.level = level.level;
+        this.speed = level.speed;
+      }
+    });
+    this.player.move(this.speed);
   }
 }
