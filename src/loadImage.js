04eab40e0ec1945c151f355f6816ed14353f510f
Ruslan Orazliev <rorazliev@outlook.com>
2021-02-11 20:18:55 -0500

    Separate helper functions into separate files

--- src/loadImage.js
+++ src/loadImage.js
@@ -1,7 +1,3 @@
-export function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 export function loadImage(src) {
   return new Promise((resolve) => {
     let image = new Image;
