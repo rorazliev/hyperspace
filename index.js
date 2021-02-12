290e6d872ed1591b0279da8acc8d978e428b7c2a
Ruslan Orazliev <rorazliev@outlook.com>
2021-02-11 20:20:47 -0500

    udate index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import meteor from './media/meteor.png';
 import ship from './media/ship.png';
 import space from './media/space.jpg';
-import Game from './src/game.js';
-import {loadImage} from './src/helpers.js';
+import Game from './src/game';
+import {loadImage} from './src/loadImage';
 
 Promise.all([
   loadImage(meteor),
   loadImage(ship),
   loadImage(space)
 ]).then((resources) => {
-  const game = new Game('canvas', resources);
+  const game = new Game(resources);
 });
