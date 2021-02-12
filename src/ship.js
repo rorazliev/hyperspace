ded39e38ffc44864c8c5d2d81290cd9e59febfff
Ruslan Orazliev <rorazliev@outlook.com>
2021-02-11 20:20:12 -0500

    integrate direction enum

--- src/ship.js
+++ src/ship.js
@@ -1,38 +1,40 @@
 import Body from './body';
+import {DIRECTION} from './direction';
+
 
 export default class Ship extends Body {
   constructor (image, canvas) {
     super(image, 0.1);
     this.x = (canvas.width / 2) - (this.width / 2);
     this.y = (canvas.height / 2) - (this.height / 2);
+    this.direction = DIRECTION.IDLE;
     this.canvas = canvas;
   }
 
-  moveLeft (speed) {
-    this.x -= speed;
-    if (this.x < 0) {
-      this.x = 0;
+  move (speed) {
+    if (this.direction == DIRECTION.UP) {
+      this.y -= speed;
+      if (this.y < 0) {
+        this.y = 0;
+      }
     }
-  }
-
-  moveRight (speed) {
-    this.x += speed;
-    if (this.x + this.width > this.canvas.width) {
-      this.x = this.canvas.width - this.width;
+    if (this.direction == DIRECTION.DOWN) {
+      this.y += speed;
+      if (this.y + this.height > this.canvas.height) {
+        this.y = this.canvas.height - this.height;
+      }
     }
-  }
-
-  moveUp (speed) {
-    this.y -= speed;
-    if (this.y < 0) {
-      this.y = 0;
+    if (this.direction == DIRECTION.LEFT) {
+      this.x -= speed;
+      if (this.x < 0) {
+        this.x = 0;
+      }
     }
-  }
-
-  moveDown (speed) {
-    this.y += speed;
-    if (this.y + this.height > this.canvas.height) {
-      this.y = thdasis.canvas.height - this.height;
+    if (this.direction == DIRECTION.RIGHT) {
+      this.x += speed;
+      if (this.x + this.width > this.canvas.width) {
+        this.x = this.canvas.width - this.width;
+      }
     }
   }
 }
