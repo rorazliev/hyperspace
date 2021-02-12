a8ed3e894b892dcca1f745b622968dc6784724fd
Ruslan Orazliev <rorazliev@outlook.com>
2021-02-11 20:19:15 -0500

    add leveling enum

---
+++ src/leveling.js
@@ -0,0 +1,52 @@
+export const LEVELING = [
+  {
+    level: 2,
+    score: 100,
+    speed: 6
+  },
+  {
+    level: 3,
+    score: 250,
+    speed: 7
+  },
+  {
+    level: 4,
+    score: 500,
+    speed: 8
+  },
+  {
+    level: 5,
+    score: 750,
+    speed: 9
+  },
+  {
+    level: 6,
+    score: 1000,
+    speed: 10
+  },
+  {
+    level: 7,
+    score: 1500,
+    speed: 12
+  },
+  {
+    level: 7,
+    score: 2000,
+    speed: 15
+  },
+  {
+    level: 8,
+    score: 2500,
+    speed: 20
+  },
+  {
+    level: 9,
+    score: 3000,
+    speed: 25
+  },
+  {
+    level: 'Insane',
+    score: 5000,
+    speed: 30
+  }
+];
